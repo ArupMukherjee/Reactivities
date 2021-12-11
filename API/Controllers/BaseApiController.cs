a74d76f62464b31767429c58cb5f93e95b0b9f35
Arup Mukherjee <mukherjeearup@gmail.com>
2021-12-10 22:05:38 -0500

    End of Section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -12,6 +14,9 @@ namespace API.Controllers
     
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
         
     }
 }
